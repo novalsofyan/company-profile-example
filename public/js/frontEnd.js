8fdf9ceaa4fc37047aa7cfff480e290a986c5b66
novalsofyan <59356604+novalsofyan@users.noreply.github.com>
2025-06-26 20:01:37 +0700

    checkpoint slider

--- public/js/frontEnd.js
+++ public/js/frontEnd.js
@@ -8,13 +8,18 @@ class FrontEndScripts {
     this.navbarHamburgerLine = document.querySelectorAll(".navbar-hamburger-line");
     this.navbarMenu = document.querySelector(".navbar-menu-wrapper");
     this.navbarMenuList = document.querySelectorAll(".navbar-menu-list");
-
+    // Card Slider Variable
+    this.cardWrapper = document.querySelector(".card-wrapper");
+    this.isDragging = false;
+    this.startX = undefined;
     this.init();
   }
 
   init() {
     // Hamburger Function
     this.hamburgerClickEvent();
+    // Card Slider Function
+    this.cardSlider();
   }
 
   // Hambuger Event Click Listener
@@ -35,9 +40,45 @@ class FrontEndScripts {
       });
     });
   }
+
+  cardSlider() {
+    if (this._dragStartHandler) {
+      this.cardWrapper.removeEventListener("touchstart", this._dragStartHandler);
+      this.cardWrapper.removeEventListener("touchmove", this._draggingHandler);
+      this.cardWrapper.removeEventListener("touchend", this._dragStopHandler);
+      this.cardWrapper.removeEventListener("mousedown", this._dragStartHandler);
+      this.cardWrapper.removeEventListener("mousemove", this._draggingHandler);
+      this.cardWrapper.removeEventListener("mouseup", this._dragStopHandler);
+      this.cardWrapper.removeEventListener("mouseleave", this._dragStopHandler);
+    }
+
+    this._dragStartHandler = (e) => {
+      this.isDragging = true;
+      this.cardWrapper.classList.add("dragging");
+      this.startX = e.pageX || e.touches[0].pageX;
+      this.startScrollLeft = this.cardWrapper.scrollLeft;
+    };
+
+    this._draggingHandler = (e) => {
+      if (!this.isDragging) return;
+      const currentX = e.pageX || e.touches[0].pageX;
+      const walk = currentX - this.startX;
+      this.cardWrapper.scrollLeft = this.startScrollLeft - walk;
+    };
+
+    this._dragStopHandler = () => {
+      this.isDragging = false;
+      this.cardWrapper.classList.remove("dragging");
+    };
+
+    this.cardWrapper.addEventListener("touchstart", this._dragStartHandler);
+    this.cardWrapper.addEventListener("touchmove", this._draggingHandler);
+    this.cardWrapper.addEventListener("touchend", this._dragStopHandler);
+    this.cardWrapper.addEventListener("mousedown", this._dragStartHandler);
+    this.cardWrapper.addEventListener("mousemove", this._draggingHandler);
+    this.cardWrapper.addEventListener("mouseup", this._dragStopHandler);
+    this.cardWrapper.addEventListener("mouseleave", this._dragStopHandler);
+  }
 }
 
-// Start Whole Script When DOM Content Loaded
-document.addEventListener("DOMContentLoaded", () => {
-  new FrontEndScripts();
-});
+new FrontEndScripts();
